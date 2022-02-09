e459daa3b01daf24e4093fd70fff0333c3391bec
terra-d <38385364+terra-d@users.noreply.github.com>
2022-02-09 22:21:52 +0900

    add posh-git

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,14 @@
-# PSFzf
+Import-Module oh-my-posh
 Import-Module PSFzf
+Import-Module posh-git
+
+# PSFzf
 Enable-PsFzfAliases
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # oh-my-posh
-Import-Module oh-my-posh
 Set-PoshPrompt -Theme atomic
+$env:POSH_GIT_ENABLED = $true
 
 # touch command to make an empty file
 function touch($filename) {New-Item -type file $filename}
