d9349f6d334e2576b4544b3f6ab35149e12d5b95
terra-d <38385364+terra-d@users.noreply.github.com>
2022-02-09 22:06:04 +0900

    add PSFzf

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,12 @@
+# PSFzf
+Import-Module PSFzf
+Enable-PsFzfAliases
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# oh-my-posh
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme atomic
+
 # touch command to make an empty file
 function touch($filename) {New-Item -type file $filename}
 
-# python3 command
-New-Alias python3 python
