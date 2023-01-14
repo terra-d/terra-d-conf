164e38bf21f11a63eb9417c0a3afdf8d9c21c755
terra-d <38385364+terra-d@users.noreply.github.com>
2023-01-15 03:03:49 +0900

    add

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -1,54 +1,57 @@
--- load vim settings
-require("settings")
+if not vim.g.vscode then
+  -- load vim settings
+  require("settings")
 
--- set up the package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+  -- set up the package manager
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  local plugins = require("plugins")
+  require("lazy").setup(plugins, {
+      root  = vim.fn.stdpath("data") .. "/lazy",
+      lockfile = "../lazy-lock.json",
+      install = {
+          missing = true,
+          colorscheme = {
+              "habamax",
+          },
+      },
+      checker = {
+          enabled = true,
+          notify = true,
+      },
+      change_detection = {
+          enabled = true,
+          notify = true,
+      },
+      diff = {
+          cmd = "git",
+      },
+      performance = {
+          cache = {
+              enabled = true,
+              path = vim.fn.stdpath("cache") .. "/lazy/cache",
+          },
+          rtp = {
+              disabled_plugins = {
+                  -- "gzip",
+                  -- "netrwPlugin",
+                  -- "tarPlugin",
+                  -- "tohtml",
+                  -- "tutor",
+                  -- "zipPlugin",
+              },
+          },
+      },
   })
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-    root  = vim.fn.stdpath("data") .. "/lazy",
-    lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
-    install = {
-        missing = true,
-        colorscheme = {
-            "habamax",
-        },
-    },
-    checker = {
-        enabled = true,
-        notify = true,
-    },
-    change_detection = {
-        enabled = true,
-        notify = true,
-    },
-    diff = {
-        cmd = "git",
-    },
-    performance = {
-        cache = {
-            enabled = true,
-            path = vim.fn.stdpath("cache") .. "/lazy/cache",
-        },
-        rtp = {
-            disabled_plugins = {
-                -- "gzip",
-                -- "netrwPlugin",
-                -- "tarPlugin",
-                -- "tohtml",
-                -- "tutor",
-                -- "zipPlugin",
-            },
-        },
-    },
-})
