8002dcf2e6012b99b41361c3889e8fa48340d5b0
terra-d <38385364+terra-d@users.noreply.github.com>
2023-01-31 02:25:27 +0900

    add

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,6 +10,9 @@ return {
   -- Showing indent lines
   {
     "lukas-reineke/indent-blankline.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
     config = require("plugin-settings.indent-blankline"),
   },
   -- Easymotion
