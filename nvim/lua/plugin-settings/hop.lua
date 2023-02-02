7b8af4ef62bfb3000358c0e375f43630d7bd8a3f
terra-d <38385364+terra-d@users.noreply.github.com>
2023-02-02 16:15:36 +0900

    modify

--- nvim/lua/plugin-settings/hop.lua
+++ nvim/lua/plugin-settings/hop.lua
@@ -7,17 +7,10 @@ function M.setup()
   local directions = require("hop.hint").HintDirection
 
   map("n", "f", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    hop.hint_char1({ current_line_only = true })
   end)
-  map('', 'F', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-  end, {remap=true})
-  map('', 'p', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-  end, {remap=true})
-  map('', 'P', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-  end, {remap=true})
+  map("n", "<leader>f", ":HopWordMW<CR>")
+  map("n", "<leader>/", ":HopPatternMW<CR>")
 end
 
 return M
