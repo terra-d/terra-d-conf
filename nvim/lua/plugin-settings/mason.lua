dd7ca4855b6077499a7cf9a2c05d3523d07e72e2
terra-d <38385364+terra-d@users.noreply.github.com>
2023-02-25 23:45:07 +0900

    modify

--- nvim/lua/plugin-settings/mason.lua
+++ nvim/lua/plugin-settings/mason.lua
@@ -13,14 +13,6 @@ local lsp_servers = {
                     -- Get the language server to recognize the `vim` global
                     globals = { "vim" },
                 },
-                workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                    enable = false,
-                },
             },
         },
     },
