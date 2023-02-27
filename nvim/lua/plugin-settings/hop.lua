22084130022494396506a7d5be45e73f477f6260
terra-d <38385364+terra-d@users.noreply.github.com>
2023-02-28 02:06:37 +0900

    add xnoremap

--- nvim/lua/plugin-settings/hop.lua
+++ nvim/lua/plugin-settings/hop.lua
@@ -4,13 +4,17 @@ local map = require("utils").map
 function M.setup()
   local hop = require("hop")
   hop.setup()
-  local directions = require("hop.hint").HintDirection
 
   map("n", "f", function()
     hop.hint_char1({ current_line_only = true })
   end)
+  map("x", "f", function()
+    hop.hint_char1({ current_line_only = true })
+  end)
   map("n", "<leader>f", ":HopWordMW<CR>")
+  map("x", "<leader>f", ":HopWordMW<CR>")
   map("n", "<leader>/", ":HopPatternMW<CR>")
+  map("x", "<leader>/", ":HopPatternMW<CR>")
 end
 
 return M
