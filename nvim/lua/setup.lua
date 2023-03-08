c93697b0271bd7ae1cc565c0491134957759526c
terra-d <38385364+terra-d@users.noreply.github.com>
2023-03-09 02:55:12 +0900

    modify

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -5,25 +5,25 @@ require("settings")
 -- set up the package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = require("plugins")
 require("lazy").setup(plugins, {
-    root  = vim.fn.stdpath("data") .. "/lazy",
-    lockfile = configration_path .. "/lazy-lock.json",
-    install = {
+    root             = vim.fn.stdpath("data") .. "/lazy",
+    lockfile         = CONFIGRATION_PATH .. "/lazy-lock.json",
+    install          = {
         missing = true,
     },
-    checker = {
+    checker          = {
         enabled = true,
         notify = true,
     },
@@ -31,24 +31,24 @@ require("lazy").setup(plugins, {
         enabled = true,
         notify = true,
     },
-    diff = {
+    diff             = {
         cmd = "git",
     },
-    performance = {
+    performance      = {
         cache = {
             enabled = true,
             path = vim.fn.stdpath("cache") .. "/lazy/cache",
         },
         rtp = {
             disabled_plugins = {
-              -- "gzip",
-              -- "matchit",
-              -- "matchparen",
-              "netrwPlugin",
-              -- "tarPlugin",
-              -- "tohtml",
-              -- "tutor",
-              -- "zipPlugin",
+                -- "gzip",
+                -- "matchit",
+                -- "matchparen",
+                "netrwPlugin",
+                -- "tarPlugin",
+                -- "tohtml",
+                -- "tutor",
+                -- "zipPlugin",
             },
         },
     },
