47fe7512acb4fde6e0e69c5998834c3b672a5289
terra-d <38385364+terra-d@users.noreply.github.com>
2023-03-14 02:25:02 +0900

    show dotfile

--- nvim/lua/plugin-settings/neo-tree.lua
+++ nvim/lua/plugin-settings/neo-tree.lua
@@ -2,17 +2,22 @@ local M = {}
 local map = require("utils").map
 
 function M.setup()
-  require("neo-tree").setup({
-    window = {
-        mappings = {
-            ["e"] = "none",
-            ["s"] = "open_split",
-            ["v"] = "open_vsplit",
-        }
-    },
-  })
+    require("neo-tree").setup({
+        window = {
+            mappings = {
+                ["e"] = "none",
+                ["s"] = "open_split",
+                ["v"] = "open_vsplit",
+            },
+        },
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+            },
+        },
+    })
 
-  map("n", "<leader>E", "<cmd>:NeoTreeShow<CR>")
+    map("n", "<leader>E", "<cmd>NeoTreeShow<CR>")
 end
 
 return M
