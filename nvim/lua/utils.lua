9ee1d870405ca62d73d2748bea09e59133ea5acb
terra-d <38385364+terra-d@users.noreply.github.com>
2023-03-15 02:19:14 +0900

    modify

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,26 +1,28 @@
 local M = {}
 
 function M.map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.keymap.set(mode, lhs, rhs, options)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 function M.to_boolean(object)
-    local object_type = type(object)
+  local object_type = type(object)
 
-    if object_type == "boolean" then
-      return object
-    elseif object == nil then
-      return false
-    elseif object_type == "number" then
-      return object ~= 0 and true or false
-    else
-      error("Invalid argument type")
-      return nil
-    end
+  if object_type == "boolean" then
+    return object
+  end
+  if object == nil then
+    return false
+  end
+  if object_type == "number" then
+    return object ~= 0 and true or false
+  end
+
+  error("Invalid argument type")
+  return nil
 end
 
 return M
