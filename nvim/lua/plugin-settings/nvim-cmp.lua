045b0400a9a03dd4c5302d2d9ab81bc80749c6f4
terra-d <38385364+terra-d@users.noreply.github.com>
2023-03-16 00:07:52 +0900

    modify

--- nvim/lua/plugin-settings/nvim-cmp.lua
+++ nvim/lua/plugin-settings/nvim-cmp.lua
@@ -29,6 +29,7 @@ function M.setup()
       },
     }),
   })
+
   cmp.setup.filetype("gitcommit", {
     sources = cmp.config.sources({
       { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -105,6 +105,7 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "petertriho/cmp-git",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
@@ -112,7 +113,6 @@ return {
       require("plugin-settings.nvim-cmp").setup()
     end,
   },
-
   -- copilot
   {
     "github/copilot.vim",
