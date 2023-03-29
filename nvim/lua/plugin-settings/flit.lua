712c51835dfff586a2ee370aaf8116a0741ca5c9
terra-d <38385364+terra-d@users.noreply.github.com>
2023-03-30 03:51:57 +0900

    modify keybinding not to wait time when to press g

--- nvim/lua/plugin-settings/flit.lua
+++ nvim/lua/plugin-settings/flit.lua
@@ -1,7 +1,7 @@
 return {
   -- Note: I don't use the `t` and `T` keys, but I've left them in here
   -- https://github.com/ggandor/flit.nvim/issues/8
-  keys = { f = "f", F = "F", t = "ggt", T = "ggT" },
+  keys = { f = "f", F = "F", t = "<M-9>", T = "<M-0>" },
   -- A string like "nv", "nvo", "o", etc.
   labeled_modes = "v",
   multiline = true,
