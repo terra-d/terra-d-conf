02ca5427fe56a9552c4b160440e70b69056ad660
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-03 04:16:20 +0900

    add

---
+++ fish/install.sh
@@ -0,0 +1,13 @@
+# installation
+sudo apt install fish
+curl -sS https://starship.rs/install.sh | sh
+
+# fish
+chsh -s /usr/bin/fish
+
+fish
+
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+echo "Please restart shell."
