712f89ae7c654a9afba6397987126adb494a36f0
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-04 23:44:50 +0900

    modify

--- fish/install.sh
+++ fish/install.sh
@@ -1,6 +1,7 @@
 # installation
-sudo apt install fish
-curl -sS https://starship.rs/install.sh | sh
+sudo pacman -S fish
+sudo pacman -S starship
+sudo pacman -S which
 
 # fish
 chsh -s /usr/bin/fish
@@ -8,4 +9,6 @@ chsh -s /usr/bin/fish
 # starship
 curl -sS https://starship.rs/install.sh | sh
 
+mkdir -p $HOME/.config/fish
+echo "SETUVAR --export XDG_CONFIG_HOME:/home/terra/terra\\x2dd\\x2dconf" >> /home/terra/.config/fish_variables
 echo "Please restart shell."
