85587ee82e4e0071df850ce45c827fef3227cc93
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-05 00:29:26 +0900

    modify

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,8 @@ local wezterm = require("wezterm")
 local config = {
 	color_scheme = "nordfox",
 	font = wezterm.font("RictyDiminishedDiscord NF"),
-	default_prog = { "wsl.exe", "--cd", "~" },
+	default_domain = "WSL:Arch",
+	default_prog = { "wsl.exe" },
 }
 
 return config
--- fish/install.sh
+++ fish/install.sh
@@ -1,14 +1,22 @@
 # installation
-sudo pacman -S fish
-sudo pacman -S starship
-sudo pacman -S which
+sudo pacman -S --needed fish
+sudo pacman -S --needed starship
+sudo pacman -S --needed which
+sudo pacman -S --needed github-cli
+sudo pacman -S --needed base-devel
+
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S google-chrome
 
 # fish
+echo `(which chsh)` | sudo tee -a /etc/shells
 chsh -s /usr/bin/fish
 
-# starship
-curl -sS https://starship.rs/install.sh | sh
-
 mkdir -p $HOME/.config/fish
+echo fish > $HOME/.config/fish/config.fish
 echo "SETUVAR --export XDG_CONFIG_HOME:/home/terra/terra\\x2dd\\x2dconf" >> /home/terra/.config/fish_variables
+
+fish -c "set -Ux STARSHIP_CONFIG $HOME/terra-d-conf/starship.toml"
+
+# finished
 echo "Please restart shell."
