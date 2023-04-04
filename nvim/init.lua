db17100651be20e9c328dd1f8860d43488682d3d
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-04 22:49:18 +0900

    modify

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,5 @@
-CONFIGRATION_PATH = "/absolute/path/of/this-directly"
-package.path = package.path .. ";" .. CONFIGRATION_PATH .. "/lua/?.lua"
+CONFIGRATION_PATH = vim.fn.stdpath("config")
 
--- keep vimscript to use both vim and VSCode
 local rcpath = CONFIGRATION_PATH .. "/rc/keymaps.vim"
 vim.cmd("source " .. rcpath)
 
