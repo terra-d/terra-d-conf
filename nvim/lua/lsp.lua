e8c484652f7639734e26f005ab0da2065555fe77
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-06 23:38:12 +0900

    modify

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,10 +2,10 @@ local M = {}
 local map = require("utils").map
 
 function M.setup()
-  map("n", "<space>e", vim.diagnostic.open_float)
-  map("n", "[d", vim.diagnostic.goto_prev)
-  map("n", "]d", vim.diagnostic.goto_next)
-  map("n", "<leader>M", vim.diagnostic.setloclist)
+  map("n", "<space>n", vim.diagnostic.open_float)
+  map("n", "<space>[", vim.diagnostic.goto_prev)
+  map("n", "<space>]", vim.diagnostic.goto_next)
+  map("n", "<space>M", vim.diagnostic.setloclist)
 
   -- Use LspAttach autocommand to only map the following keys
   -- after the language server attaches to the current buffer
@@ -18,20 +18,20 @@ function M.setup()
       -- Buffer local mappings.
       -- See `:help vim.lsp.*` for documentation on any of the below functions
       local opts = { buffer = ev.buf }
-      map("n", "gD", vim.lsp.buf.declaration, opts)
-      map("n", "gd", vim.lsp.buf.definition, opts)
+      map("n", "<space>D", vim.lsp.buf.declaration, opts)
+      map("n", "<space>d", vim.lsp.buf.definition, opts)
       map("n", "N", vim.lsp.buf.hover, opts)
-      map("n", "gi", vim.lsp.buf.implementation, opts)
-      map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+      map("n", "<space>i", vim.lsp.buf.implementation, opts)
+      map("n", "<C-n>", vim.lsp.buf.signature_help, opts)
       map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
       map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
       map("n", "<space>wl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       end, opts)
-      map("n", "<space>D", vim.lsp.buf.type_definition, opts)
-      map("n", "<space>rn", vim.lsp.buf.rename, opts)
-      map("n", "<space>ca", vim.lsp.buf.code_action, opts)
-      map("n", "gr", vim.lsp.buf.references, opts)
+      map("n", "<space>t", vim.lsp.buf.type_definition, opts)
+      map("n", "<space>R", vim.lsp.buf.rename, opts)
+      map("n", "<space>.", vim.lsp.buf.code_action, opts)
+      map("n", "<space>r", vim.lsp.buf.references, opts)
       map("n", "<space>f", function()
         vim.lsp.buf.format({ async = true })
       end, opts)
