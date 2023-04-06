4727f07f73a60bce93e3043ee6e7f0fda02842f4
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-07 01:18:32 +0900

    change keybinding for split

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,22 @@
 local wezterm = require("wezterm")
 
+local wsl_domains = wezterm.default_wsl_domains()
+for _, dom in ipairs(wsl_domains) do
+    dom.default_cwd = "~"
+end
+
 local config = {
 	color_scheme = "nordfox",
 	font = wezterm.font("RictyDiminishedDiscord NF"),
 	default_domain = "WSL:Arch",
 	default_prog = { "wsl.exe" },
+	keys = {
+		{
+			key = "s",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+		}
+	},
 }
 
 return config
