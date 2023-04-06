29a30faff948c386c1cbc006b2c5d61d33274e78
terra-d <38385364+terra-d@users.noreply.github.com>
2023-04-07 01:56:15 +0900

    add rg

--- fish/install.sh
+++ fish/install.sh
@@ -1,15 +1,16 @@
 # installation
-sudo -I pacman -S --needed --noconfirm base-devel
-sudo -I pacman -S --needed --noconfirm bat
-sudo -I pacman -S --needed --noconfirm fd
-sudo -I pacman -S --needed --noconfirm fish
-sudo -I pacman -S --needed --noconfirm fzf
-sudo -I pacman -S --needed --noconfirm github-cli
-sudo -I pacman -S --needed --noconfirm starship
-sudo -I pacman -S --needed --noconfirm tk
-sudo -I pacman -S --needed --noconfirm wget
-sudo -I pacman -S --needed --noconfirm which
-sudo -I pacman -S --needed --noconfirm zip unzip
+sudo -E pacman -S --needed --noconfirm base-devel
+sudo -E pacman -S --needed --noconfirm bat
+sudo -E pacman -S --needed --noconfirm fd
+sudo -E pacman -S --needed --noconfirm fish
+sudo -E pacman -S --needed --noconfirm fzf
+sudo -E pacman -S --needed --noconfirm github-cli
+sudo -E pacman -S --needed --noconfirm ripgreg
+sudo -E pacman -S --needed --noconfirm starship
+sudo -E pacman -S --needed --noconfirm tk
+sudo -E pacman -S --needed --noconfirm wget
+sudo -E pacman -S --needed --noconfirm which
+sudo -E pacman -S --needed --noconfirm zip unzip
 
 curl https://rtx.pub/install.sh | sh
 
@@ -26,6 +27,7 @@ fisher update
 
 # rtx
 fish -c "set -Ux STARSHIP_CONFIG $HOME/terra-d-conf/starship.toml"
+fish -c "set -Ux RIPGREP_CONFIG_PATH $HOME/.config/.ripgreprc"
 fish -c "fish_add_path $HOME/.local/share/rtx/bin"
 fish -c "rtx install awscli@2"
 fish -c "rtx global awscli@2"
