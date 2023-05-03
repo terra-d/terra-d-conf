65aaf8313def9188096f38ea873433b5252bf011
terra-d <38385364+terra-d@users.noreply.github.com>
2023-05-04 01:30:19 +0900

    format

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -5,52 +5,52 @@ require("settings")
 -- set up the package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = require("plugins")
 require("lazy").setup(plugins, {
-    root = vim.fn.stdpath("data") .. "/lazy",
-    lockfile = CONFIGRATION_PATH .. "/lazy-lock.json",
-    install = {
-        missing = true,
-        colorscheme = { "nightfox" },
+  root = vim.fn.stdpath("data") .. "/lazy",
+  lockfile = CONFIGRATION_PATH .. "/lazy-lock.json",
+  install = {
+    missing = true,
+    colorscheme = { "nightfox" },
+  },
+  checker = {
+    enabled = true,
+    notify = true,
+  },
+  change_detection = {
+    enabled = true,
+    notify = true,
+  },
+  diff = {
+    cmd = "git",
+  },
+  performance = {
+    cache = {
+      enabled = true,
+      path = vim.fn.stdpath("cache") .. "/lazy/cache",
     },
-    checker = {
-        enabled = true,
-        notify = true,
+    rtp = {
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
     },
-    change_detection = {
-        enabled = true,
-        notify = true,
-    },
-    diff = {
-        cmd = "git",
-    },
-    performance = {
-        cache = {
-            enabled = true,
-            path = vim.fn.stdpath("cache") .. "/lazy/cache",
-        },
-        rtp = {
-            disabled_plugins = {
-                -- "gzip",
-                -- "matchit",
-                -- "matchparen",
-                "netrwPlugin",
-                -- "tarPlugin",
-                -- "tohtml",
-                -- "tutor",
-                -- "zipPlugin",
-            },
-        },
-    },
+  },
 })
