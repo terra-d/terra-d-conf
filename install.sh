890937c2e9e4abfeac5b39b51094e204a6d771ed
terra-d <38385364+terra-d@users.noreply.github.com>
2024-01-01 11:22:29 +0900

    m

--- install.sh
+++ install.sh
@@ -1,26 +1,47 @@
+#!/bin/bash
+
 # installation
-sudo -E pacman -Syu
-sudo -E pacman -S --needed --noconfirm base-devel
-sudo -E pacman -S --needed --noconfirm bat
-sudo -E pacman -S --needed --noconfirm docker
-sudo -E pacman -S --needed --noconfirm docker-compose
-sudo -E pacman -S --needed --noconfirm fd
-sudo -E pacman -S --needed --noconfirm fish
-sudo -E pacman -S --needed --noconfirm fzf
-sudo -E pacman -S --needed --noconfirm github-cli
-sudo -E pacman -S --needed --noconfirm helix
-sudo -E pacman -S --needed --noconfirm nushell
-sudo -E pacman -S --needed --noconfirm ripgreg
-sudo -E pacman -S --needed --noconfirm starship
-sudo -E pacman -S --needed --noconfirm tk
-sudo -E pacman -S --needed --noconfirm wget
-sudo -E pacman -S --needed --noconfirm which
-sudo -E pacman -S --needed --noconfirm zip unzip
+if [ "$(uname)" == 'Darwin' ]; then
+  # install brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 
-curl https://rtx.jdx.dev/install.sh | sh
+  # install packages
+  brew install bat
+  brew install docker
+  brew install fd
+  brew install fish
+  brew install fzf
+  brew install gh
+  brew install helix
+  brew install nushell
+  brew install ripgreg
+  brew install starship
+else
+  sudo -E pacman -Syu
+  sudo -E pacman -S --needed --noconfirm base-devel
+  sudo -E pacman -S --needed --noconfirm bat
+  sudo -E pacman -S --needed --noconfirm docker
+  sudo -E pacman -S --needed --noconfirm docker-compose
+  sudo -E pacman -S --needed --noconfirm fd
+  sudo -E pacman -S --needed --noconfirm fish
+  sudo -E pacman -S --needed --noconfirm fzf
+  sudo -E pacman -S --needed --noconfirm github-cli
+  sudo -E pacman -S --needed --noconfirm helix
+  sudo -E pacman -S --needed --noconfirm nushell
+  sudo -E pacman -S --needed --noconfirm ripgreg
+  sudo -E pacman -S --needed --noconfirm starship
+  sudo -E pacman -S --needed --noconfirm tk
+  sudo -E pacman -S --needed --noconfirm wget
+  sudo -E pacman -S --needed --noconfirm which
+  sudo -E pacman -S --needed --noconfirm zip unzip
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S google-chrome
+  curl https://rtx.jdx.dev/install.sh | sh
+
+  git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+  yay -S google-chrome
+fi
+
 
 # fish
 echo `(which chsh)` | sudo tee -a /etc/shells
@@ -30,6 +51,7 @@ chsh -s /usr/bin/fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher update
 
+fish -c "set -Ux WEZTERM_CONFIG_FILE $HOME/.config/.wezterm.lua"
 fish -c "set -Ux STARSHIP_CONFIG $HOME/terra-d-conf/starship.toml"
 fish -c "set -Ux RIPGREP_CONFIG_PATH $HOME/.config/.ripgreprc"
 fish -c "set -Ux PIPENV_VENV_IN_PROJECT 1"
