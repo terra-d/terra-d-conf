20cb0554152a7d6bd0c46987d316a1098ddfc97b
Daichi Manabe <daichi.manabe.gw@fujifilm.com>
2024-01-05 15:21:08 +0900

    modify options

--- nvim/lua/plugin-settings/neo-tree.lua
+++ nvim/lua/plugin-settings/neo-tree.lua
@@ -2,22 +2,23 @@ local M = {}
 local noremap = require("utils").noremap
 
 function M.setup()
-    require("neo-tree").setup({
-        window = {
-            mappings = {
-                ["e"] = "none",
-                ["s"] = "open_split",
-                ["v"] = "open_vsplit",
-            },
-        },
-        filesystem = {
-            filtered_items = {
-                hide_dotfiles = false,
-            },
-        },
-    })
+  require("neo-tree").setup({
+    use_libuv_file_watcher = true,
+    window = {
+      mappings = {
+        ["e"] = "none",
+        ["s"] = "open_split",
+        ["v"] = "open_vsplit",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  })
 
-    noremap("n", "<leader>E", "<cmd>NeoTreeShow<CR>")
+  noremap("n", "<leader>E", "<cmd>NeoTreeShow<CR>")
 end
 
 return M
