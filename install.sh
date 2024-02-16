2bcf91755cf6621633eb77bf38e17b0be6236fe4
terra-d <38385364+terra-d@users.noreply.github.com>
2024-02-16 21:13:22 +0900

    modify

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ else
   sudo -E pacman -S --needed --noconfirm which
   sudo -E pacman -S --needed --noconfirm zip unzip
 
-  curl https://rtx.jdx.dev/install.sh | sh
+  curl https://mise.run | sh
 
   git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
   yay -S google-chrome
@@ -51,10 +51,9 @@ chsh -s /usr/bin/fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher update
 
-fish -c "set -Ux STARSHIP_CONFIG $HOME/terra-d-conf/starship.toml"
 fish -c "set -Ux RIPGREP_CONFIG_PATH $HOME/.config/.ripgreprc"
 fish -c "set -Ux PIPENV_VENV_IN_PROJECT 1"
-fish -c "fish_add_path $HOME/.local/share/rtx/bin"
+fish -c "fish_add_path $HOME/.local/share/mise/bin"
 if [ "$(uname)" == 'Darwin' ]; then
   fish -c "fish_add_path /Applications/WezTerm.app/Contents/MacOS"
 fi
@@ -63,16 +62,16 @@ fi
 mkdir -p ~/.cache/starship
 nu -c "starship init nu | save -f ~/.cache/starship/init.nu"
 
-# rtx
-fish -c "rtx install awscli@2"
-fish -c "rtx global awscli@2"
-fish -c "rtx install lua@5"
-fish -c "rtx global lua@5"
-fish -c "rtx install node@18"
-fish -c "rtx global node@18"
-fish -c "rtx install python@3.11"
-fish -c "rtx global python@3.11"
-fish -c "rtx install rust@1"
-fish -c "rtx global rust@1"
+# mise
+fish -c "mise install awscli@2"
+fish -c "mise global awscli@2"
+fish -c "mise install lua@5"
+fish -c "mise global lua@5"
+fish -c "mise install node@20"
+fish -c "mise global node@20"
+fish -c "mise install python@3.11"
+fish -c "mise global python@3.11"
+fish -c "mise install rust@1"
+fish -c "mise global rust@1"
 
 echo "Please restart shell."
