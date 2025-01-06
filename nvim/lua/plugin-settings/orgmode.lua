c3b0797852ad8d2417c30f13c29d62e947a56503
terra-d <38385364+terra-d@users.noreply.github.com>
2025-01-07 06:35:09 +0900

    m

---
+++ nvim/lua/plugin-settings/orgmode.lua
@@ -0,0 +1,18 @@
+local M = {}
+-- local noremap = require("utils").noremap
+local orgmode = require("orgmode")
+
+function M.setup()
+  orgmode.setup({
+    org_agenda_files = "~/orgfiles/**/*",
+    org_default_notes_file = "~/orgfiles/refile.org",
+    org_todo_keywords = { "TODO", "DOING", "REVIEWING", "DELEGATED", "|", "DONE", "DEFERRED" },
+    org_todo_keyword_faces = {
+      REVIEWING = ":foreground blue :weight bold",
+      DELEGATED = ":background #FFFFFF :slant italic :underline on",
+      DEFERRED = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
+    },
+  })
+end
+
+return M
