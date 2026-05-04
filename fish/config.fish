221925c569fea579cda5eb6913ff3f0629c8a5a0
terra-d <38385364+terra-d@users.noreply.github.com>
2026-05-05 03:07:15 +0900

    Simplify fish configuration by removing explicit path to mise

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if test (uname -s) = "Darwin"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-  ~/.local/bin/mise activate fish | source
+  mise activate fish | source
 end
 
 starship init fish | source
